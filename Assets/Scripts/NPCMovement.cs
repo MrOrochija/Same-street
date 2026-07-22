baa811492d1dd195aaebf7cff8a77eef1fe3f12d
Orochi <danielsnikolajevs3@gmail.com>
2026-07-22 17:14:33 +0300

    add player movement

---
+++ Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,146 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMovement : MonoBehaviour
+{
+    private List<NPCStore.NumberGroup> groups;
+    private Transform pointsParent;
+
+    private int currentGroupIndex = 0;
+    private bool isWaiting = false;
+
+    private Transform currentTargetTransform;
+    private NPCStore.NumberData currentTargetData;
+
+    private Animator anim;
+    private string lastDirection = "down";
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Init(List<NPCStore.NumberGroup> allowedGroups, Transform parentTransform)
+    {
+        groups = allowedGroups;
+        pointsParent = parentTransform;
+        currentGroupIndex = 0;
+
+        MoveToNextTarget();
+    }
+
+    private void Update()
+    {
+        if (isWaiting || currentTargetTransform == null)
+        {
+            if (anim != null) anim.SetBool("isMoving", false);
+            return;
+        }
+
+        Vector3 targetPosition = new Vector3(
+            currentTargetTransform.position.x,
+            currentTargetTransform.position.y,
+            transform.position.z
+        );
+
+        Vector2 directionVector = targetPosition - transform.position;
+        float distance = Vector2.Distance(transform.position, targetPosition);
+
+        if (distance >= 0.05f)
+        {
+            DetermineDirection(directionVector);
+            SetAnimation();
+            if (anim != null) anim.SetBool("isMoving", true);
+
+            transform.position = Vector3.MoveTowards(
+                transform.position, 
+                targetPosition, 
+                3f * Time.deltaTime
+            );
+        }
+        else
+        {
+            if (anim != null) anim.SetBool("isMoving", false);
+            StartCoroutine(ProcessArrival());
+        }
+    }
+
+    private void DetermineDirection(Vector2 moveInput)
+    {
+        if (Mathf.Abs(moveInput.x) > Mathf.Abs(moveInput.y))
+        {
+            if (moveInput.x > 0) lastDirection = "right";
+            else if (moveInput.x < 0) lastDirection = "left";
+        }
+        else
+        {
+            if (moveInput.y > 0) lastDirection = "up";
+            else if (moveInput.y < 0) lastDirection = "down";
+        }
+    }
+
+    private void SetAnimation()
+    {
+        if (anim == null) return;
+
+        anim.SetBool("isLeft", lastDirection == "left");
+        anim.SetBool("isRight", lastDirection == "right");
+        anim.SetBool("isUp", lastDirection == "up");
+        anim.SetBool("isDown", lastDirection == "down");
+    }
+
+    private void MoveToNextTarget()
+    {
+        if (groups == null || groups.Count == 0)
+        {
+            currentTargetTransform = null;
+            return;
+        }
+
+        if (currentGroupIndex >= groups.Count)
+        {
+            currentGroupIndex = 0;
+        }
+
+        NPCStore.NumberGroup currentGroup = groups[currentGroupIndex];
+
+        if (currentGroup.numberList == null || currentGroup.numberList.Count == 0)
+        {
+            currentTargetTransform = null;
+            return;
+        }
+
+        int randomIndex = Random.Range(0, currentGroup.numberList.Count);
+        currentTargetData = currentGroup.numberList[randomIndex];
+
+        string targetName = "Pos" + currentTargetData.number;
+        currentTargetTransform = pointsParent.Find(targetName);
+
+        if (currentTargetTransform == null)
+        {
+            currentGroupIndex = 0;
+            MoveToNextTarget();
+        }
+    }
+
+    private IEnumerator ProcessArrival()
+    {
+        isWaiting = true;
+        if (anim != null) anim.SetBool("isMoving", false);
+
+        if (currentTargetData != null)
+        {
+            if (currentTargetData.wait)
+            {
+                yield return new WaitForSeconds(5f);
+            }
+
+            currentGroupIndex = currentTargetData.number;
+        }
+
+        isWaiting = false;
+
+        MoveToNextTarget();
+    }
+}
---
+++ Assets/Scripts/NPCStore.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class NPCStore : MonoBehaviour
+{
+    public int i = 0;
+
+    [System.Serializable]
+    public class NumberData
+    {
+        public int number;
+        public bool wait;
+    }
+
+    [System.Serializable]
+    public class NumberGroup
+    {
+        public List<NumberData> numberList;
+    }
+
+    public List<NumberGroup> allowedGroups;
+
+    public GameObject[] NPC;
+    private GameObject spawn;
+
+    void Start()
+    {
+        Transform spawnTransform = transform.Find("Spawn");
+
+        if (spawnTransform != null)
+        {
+            spawn = spawnTransform.gameObject;
+            SpawnRandomNPC();
+        }
+    }
+
+    public void SpawnRandomNPC()
+    {
+        if (spawn == null || NPC.Length == 0) return;
+
+        int randomIndex = Random.Range(0, NPC.Length);
+
+        GameObject randomNPC = Instantiate(
+            NPC[randomIndex], 
+            spawn.transform.position, 
+            spawn.transform.rotation, 
+            transform
+        );
+
+        randomNPC.SetActive(true);
+
+        NPCMovement movement = randomNPC.GetComponent<NPCMovement>();
+        if (movement != null)
+        {
+            movement.Init(allowedGroups, transform);
+        }
+    }
+}
